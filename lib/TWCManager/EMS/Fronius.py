42d2de45e2cce6b0ff249a084b3ea53f230f1b6d
Nathan Gardiner <ngardiner@gmail.com>
2019-09-18 22:45:10 +1000

    Create Fronius.py
    Fronius solar.API integration template

---
+++ lib/TWCManager/EMS/Fronius.py
@@ -0,0 +1,35 @@
+# Fronius Datamanager Solar.API Integration (Inverter Web Interface)
+
+class Fronius:
+
+  consumedW   = 0
+  generatedW  = 0
+  importW     = 0
+  exportW     = 0
+  voltage     = 0
+
+  def __init__(self, serverIP):
+    self.serverIP = serverIP
+
+  def getConsumed():
+    return self.consumedW
+
+  def getGenerated():
+    return self.generatedW
+    
+  def getInverterData():
+    url = http://" + self.serverIP + "/solar_api/v1/GetInverterRealtimeData.cgi?Scope=Device&DeviceID=1&DataCollection=CommonInverterData"
+    r = requests.get(url, timeout=60)
+	  r.raise_for_status()
+	  jsondata = r.json()
+
+  def getMeterData():
+      url = "http://" + self.serverIP + "/solar_api/v1/GetMeterRealtimeData.cgi?Scope=Device&DeviceId=0"
+      r = requests.get(url, timeout=60)
+	    r.raise_for_status()
+	    jsondata = r.json()
+
+  def update():
+    inverterData = getInverterData()
+    meterData = getMeterData()
+    
