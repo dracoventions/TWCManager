af40b9222d831dcf0c70f141ffd2447cdf09f692
Nathan Gardiner <ngardiner@gmail.com>
2019-10-03 00:01:02 +1000

    Update MQTTStatus.py
    Defined setStatus function for publishing MQTT status

--- lib/TWCManager/Status/MQTTStatus.py
+++ lib/TWCManager/Status/MQTTStatus.py
@@ -2,10 +2,20 @@
 
 class MQTTStatus:
 
-  def __init__(self, serverIP, topicPrefix):
+  import paho.mqtt.client as mqtt
+  
+  status = false
+  serverIP = None
+  topicPrefix = None
+  
+  def __init__(self, status, serverIP, topicPrefix):
+    self.status = status
     self.serverIP = serverIP
     self.topicPrefix = topicPrefix
     
   def setStatus(key, value):
-    if (self.mqttBrokerIP):
-      publish.single(self.topicPrefix+"/"+key, payload=value, hostname=self.serverIP
+    if (self.status):
+        client = mqtt.Client("P1")
+        client.connect(self.serverIP)
+        client.publish(self.topicPrefix+"/"+key, payload=value)
+        client.disconnect()
