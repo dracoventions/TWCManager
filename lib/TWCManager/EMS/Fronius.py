ba6a3b5c0c2c50b6aee0e7f747c232cf0081cade
Nathan Gardiner <ngardiner@gmail.com>
2019-10-02 23:15:58 +1000

    Update Fronius.py
    Updated indentation

--- lib/TWCManager/EMS/Fronius.py
+++ lib/TWCManager/EMS/Fronius.py
@@ -18,16 +18,16 @@ class Fronius:
     return self.generatedW
     
   def getInverterData():
-    url = http://" + self.serverIP + "/solar_api/v1/GetInverterRealtimeData.cgi?Scope=Device&DeviceID=1&DataCollection=CommonInverterData"
+    url = "http://" + self.serverIP + "/solar_api/v1/GetInverterRealtimeData.cgi?Scope=Device&DeviceID=1&DataCollection=CommonInverterData"
     r = requests.get(url, timeout=60)
-	  r.raise_for_status()
-	  jsondata = r.json()
+    r.raise_for_status()
+    jsondata = r.json()
 
   def getMeterData():
-      url = "http://" + self.serverIP + "/solar_api/v1/GetMeterRealtimeData.cgi?Scope=Device&DeviceId=0"
-      r = requests.get(url, timeout=60)
-	    r.raise_for_status()
-	    jsondata = r.json()
+    url = "http://" + self.serverIP + "/solar_api/v1/GetMeterRealtimeData.cgi?Scope=Device&DeviceId=0"
+    r = requests.get(url, timeout=60)
+    r.raise_for_status()
+    jsondata = r.json()
 
   def update():
     inverterData = getInverterData()
