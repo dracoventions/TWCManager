be2eca9b351035ed24d7f86f06d14f245e2d4e69
Nathan Gardiner <ngardiner@gmail.com>
2019-10-04 23:13:06 +1000

    Update HASS.py
    HASS plugin variables

--- lib/TWCManager/EMS/HASS.py
+++ lib/TWCManager/EMS/HASS.py
@@ -1,5 +1,16 @@
 class HASS:
 
-  def __init__(self, serverIP):
+  import requests
+  
+  apiKey = None
+  debugLevel = 0
+  status = False
+  serverIP = None
+  serverPort = 8123
+  timeout = 2
+  
+  def __init__(self, status, serverIP, serverPort):
+    self.status = status
     self.serverIP = serverIP
+    self.serverPort = serverPort
 
