08d4f121ccc25fa55b8721ca96f84d5cd47cf8ac
Nathan Gardiner <ngardiner@gmail.com>
2019-10-04 23:29:14 +1000

    Update HASS.py
    HASS module structure

--- lib/TWCManager/EMS/HASS.py
+++ lib/TWCManager/EMS/HASS.py
@@ -2,12 +2,16 @@ class HASS:
 
   import requests
   
-  apiKey = None
-  debugLevel = 0
-  status = False
-  serverIP = None
-  serverPort = 8123
-  timeout = 2
+  apiKey      = None
+  cacheTime   = 60
+  consumedW   = 0
+  debugLevel  = 0
+  generatedW  = 0
+  lastFetch   = 0
+  status      = False
+  serverIP    = None
+  serverPort  = 8123
+  timeout     = 2
   
   def __init__(self, status, serverIP, serverPort, apiKey, debugLevel):
     self.status = status
@@ -15,3 +19,33 @@ class HASS:
     self.serverPort = serverPort
     self.apikey = apiKey
     self.debugLevel = debugLevel
+    
+  def getConsumption(self):
+    return self.consumedW
+
+  def getGeneration(self):
+    return self.generatedW
+  
+  def getAPIValue(self, entity):
+    url = "http://" + self.serverIP + ":" + self.serverPort + "/api/states/" + entity
+    headers = {
+        'Authorization': 'Bearer ' + self.apiKey,
+        'content-type': 'application/json'
+    }
+
+    try:
+        httpResponse = requests.get(url, headers=headers)
+    except requests.exceptions.ConnectionError as e: 
+        print("Error connecting to HomeAssistant")
+        print(e)
+        return 0
+
+    jsonResponse = httpResponse.json() if httpResponse and httpResponse.status_code == 200 else None
+
+    if jsonResponse:
+        return jsonResponse["state"]
+    else:
+        return None
+
+  def update(self):
+    # Update
