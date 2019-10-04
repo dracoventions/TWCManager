13b9dd0772175397a3232c714aac8ff342c5c58c
Nathan Gardiner <ngardiner@gmail.com>
2019-10-04 17:04:45 +1000

    Update Fronius.py
    Bugfix

--- lib/TWCManager/EMS/Fronius.py
+++ lib/TWCManager/EMS/Fronius.py
@@ -11,25 +11,25 @@ class Fronius:
   def __init__(self, serverIP):
     self.serverIP = serverIP
 
-  def getConsumed():
+  def getConsumed(self):
     return self.consumedW
 
-  def getGenerated():
+  def getGenerated(self):
     return self.generatedW
     
-  def getInverterData():
+  def getInverterData(self):
     url = "http://" + self.serverIP + "/solar_api/v1/GetInverterRealtimeData.cgi?Scope=Device&DeviceID=1&DataCollection=CommonInverterData"
     r = requests.get(url, timeout=60)
     r.raise_for_status()
     jsondata = r.json()
 
-  def getMeterData():
+  def getMeterData(self):
     url = "http://" + self.serverIP + "/solar_api/v1/GetMeterRealtimeData.cgi?Scope=Device&DeviceId=0"
     r = requests.get(url, timeout=60)
     r.raise_for_status()
     jsondata = r.json()
 
-  def update():
+  def update(self):
     inverterData = getInverterData()
     meterData = getMeterData()
     
