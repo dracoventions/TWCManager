eef8f509f94e99c7aff1bc521b753f85a6f73ec7
Nathan Gardiner <ngardiner@gmail.com>
2019-10-11 22:12:49 +1100

    Moved Tesla Vehicle API to individual module

--- TWCManager.py
+++ TWCManager.py
@@ -47,6 +47,7 @@ from lib.TWCManager.EMS.Fronius import Fronius
 from lib.TWCManager.EMS.HASS import HASS
 from lib.TWCManager.Status.HASSStatus import HASSStatus
 from lib.TWCManager.Status.MQTTStatus import MQTTStatus
+from lib.TWCManager.Vehicle.TeslaAPI import CarApiVehicle
 
 ##########################
 # Load Configuration File
@@ -1132,130 +1133,6 @@ def check_green_energy():
 
 #
 # End functions
-#
-##############################
-
-
-##############################
-#
-# Begin CarApiVehicle class
-#
-
-class CarApiVehicle:
-    ID = None
-
-    firstWakeAttemptTime = 0
-    lastWakeAttemptTime = 0
-    delayNextWakeAttempt = 0
-
-    lastErrorTime = 0
-    stopAskingToStartCharging = False
-    lat = 10000
-    lon = 10000
-
-    def __init__(self, ID):
-        self.ID = ID
-
-    def ready(self):
-        global config, carApiLastErrorTime, carApiErrorRetryMins
-
-        if(time.time() - self.lastErrorTime < carApiErrorRetryMins*60):
-            # It's been under carApiErrorRetryMins minutes since the car API
-            # generated an error on this vehicle. Return that car is not ready.
-            if(config['config']['debugLevel'] >= 8):
-                print(time_now() + ': Vehicle ' + str(self.ID)
-                    + ' not ready because of recent lastErrorTime '
-                    + str(self.lastErrorTime))
-            return False
-
-        if(self.firstWakeAttemptTime == 0 and time.time() - self.lastWakeAttemptTime < 2*60):
-            # Less than 2 minutes since we successfully woke this car, so it
-            # should still be awake.  Tests on my car in energy saver mode show
-            # it returns to sleep state about two minutes after the last command
-            # was issued.  Times I've tested: 1:35, 1:57, 2:30
-            return True
-
-        if(config['config']['debugLevel'] >= 8):
-            print(time_now() + ': Vehicle ' + str(self.ID)
-                + " not ready because it wasn't woken in the last 2 minutes.")
-        return False
-
-    def update_location(self):
-        global carApiLastErrorTime, carApiTransientErrors
-
-        if(self.ready() == False):
-            return False
-
-        apiResponseDict = {}
-
-        cmd = 'curl -s -m 60 -H "accept: application/json" -H "Authorization:Bearer ' + \
-              carApiBearerToken + \
-              '" "https://owner-api.teslamotors.com/api/1/vehicles/' + \
-              str(self.ID) + '/data_request/drive_state"'
-
-        # Retry up to 3 times on certain errors.
-        for retryCount in range(0, 3):
-            if(config['config']['debugLevel'] >= 8):
-                print(time_now() + ': Car API cmd', cmd)
-            try:
-                apiResponseDict = json.loads(run_process(cmd).decode('ascii'))
-                # This error can happen here as well:
-                #   {'response': {'reason': 'could_not_wake_buses', 'result': False}}
-                # This one is somewhat common:
-                #   {'response': None, 'error': 'vehicle unavailable: {:error=>"vehicle unavailable:"}', 'error_description': ''}
-            except json.decoder.JSONDecodeError:
-                pass
-
-            try:
-                if(config['config']['debugLevel'] >= 4):
-                    print(time_now() + ': Car API vehicle GPS location', apiResponseDict, '\n')
-
-                if('error' in apiResponseDict):
-                    foundKnownError = False
-                    error = apiResponseDict['error']
-                    for knownError in carApiTransientErrors:
-                        if(knownError == error[0:len(knownError)]):
-                            # I see these errors often enough that I think
-                            # it's worth re-trying in 1 minute rather than
-                            # waiting carApiErrorRetryMins minutes for retry
-                            # in the standard error handler.
-                            if(config['config']['debugLevel'] >= 1):
-                                print(time_now() + ": Car API returned '"
-                                      + error
-                                      + "' when trying to get GPS location.  Try again in 1 minute.")
-                            time.sleep(60)
-                            foundKnownError = True
-                            break
-                    if(foundKnownError):
-                        continue
-
-                response = apiResponseDict['response']
-
-                # A successful call to drive_state will not contain a
-                # response['reason'], so we check if the 'reason' key exists.
-                if('reason' in response and response['reason'] == 'could_not_wake_buses'):
-                    # Retry after 5 seconds.  See notes in car_api_charge where
-                    # 'could_not_wake_buses' is handled.
-                    time.sleep(5)
-                    continue
-
-                self.lat = response['latitude']
-                self.lon = response['longitude']
-            except (KeyError, TypeError):
-                # This catches cases like trying to access
-                # apiResponseDict['response'] when 'response' doesn't exist in
-                # apiResponseDict.
-                if(config['config']['debugLevel'] >= 1):
-                    print(time_now() + ": ERROR: Can't get GPS location of vehicle " + str(self.ID) + \
-                          ".  Will try again later.")
-                self.lastErrorTime = time.time()
-                return False
-
-            return True
-
-
-#
-# End CarApiVehicle class
 #
 ##############################
 
