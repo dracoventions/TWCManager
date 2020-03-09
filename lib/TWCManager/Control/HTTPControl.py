69e870fcced928a15cc9c11d53e8a828a38659aa
Nathan Gardiner <ngardiner@gmail.com>
2020-03-09 12:19:59 +1100

    version number bump

--- lib/TWCManager/Control/HTTPControl.py
+++ lib/TWCManager/Control/HTTPControl.py
@@ -154,7 +154,7 @@ class HTTPControlHandler(BaseHTTPRequestHandler):
     <p>&nbsp;</p>
     <nav class='navbar fixed-top navbar-dark bg-dark' role='navigation'>
       <a class='navbar-brand' href='/'>TWCManager</a>
-      <link rel='icon' type='image/png' href='https://raw.githubusercontent.com/ngardiner/TWCManager/master/tree/v1.1.5/html/favicon.png'>
+      <link rel='icon' type='image/png' href='https://raw.githubusercontent.com/ngardiner/TWCManager/master/tree/v1.1.8/html/favicon.png'>
       <ul class="navbar-nav mr-auto">
         <li class="nav-item active">
           <a class="nav-link" href="#">Home</a>
@@ -175,7 +175,7 @@ class HTTPControlHandler(BaseHTTPRequestHandler):
           <a class='nav-link' href='https://github.com/ngardiner/TWCManager'>GitHub</a>
         </li>
       </ul>
-      <span class="navbar-text">v1.1.5</span>
+      <span class="navbar-text">v1.1.8</span>
     </nav>"""
     return page
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_namespace_packages
 setup(
     name="TWCManager",
-    version="1.1.7",
+    version="1.1.8",
     package_dir={"": "lib"},
     packages=find_namespace_packages(where="lib"),
 
