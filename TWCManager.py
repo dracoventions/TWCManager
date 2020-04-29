e8d3780f284e5bdf5e49006b7687c0f6a57aba03
Nathan Gardiner <ngardiner@gmail.com>
2020-04-29 10:37:36 +1000

    Incorrect number of parameters for fakeMaster == 2 lifetime kWh update

--- TWCManager.py
+++ TWCManager.py
@@ -1287,7 +1287,7 @@ while True:
                     )
 
                     # Record counter values for Slave TWC
-                    master.updateSlaveLifetime(senderID[0], senderID[1], kWhCounter, voltsPhaseA, voltsPhaseB, voltsPhaseC)
+                    master.updateSlaveLifetime(senderID, kWhCounter, voltsPhaseA, voltsPhaseB, voltsPhaseC)
 
                 if foundMsgMatch == False:
                     debugLog(1, "***UNKNOWN MESSAGE from master: " + hex_str(msg))
