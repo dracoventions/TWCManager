14382ba04ca3faf957398955956c8d3807e20566
Mike Bishop <mbishop@evequefou.be>
2020-05-27 15:02:30 -0400

    Ask again after raising limits

--- lib/TWCManager/Vehicle/TeslaAPI.py
+++ lib/TWCManager/Vehicle/TeslaAPI.py
@@ -816,6 +816,7 @@ class TeslaAPI:
             # only wake if they might be able to charge further now
             if wasAtHome and limit > (lastApplied if lastApplied != -1 else outside):
                 needToWake = True
+                vehicle.stopAskingToStartCharging = False
             if (
                 wasAtHome
                 and (
