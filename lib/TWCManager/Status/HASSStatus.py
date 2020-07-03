34fc0f78a71d09b82757abbc55ae05cb8aa262d4
Patrick Matusz <patrick@matusz.ch>
2020-07-03 22:08:57 +0200

    Creating message queue for HASS status reporting

--- lib/TWCManager/Status/HASSStatus.py
+++ lib/TWCManager/Status/HASSStatus.py
@@ -7,7 +7,9 @@ from ww import f
 class HASSStatus:
 
     import time
+    import threading    
     import requests
+
 
     apiKey = None
     config = None
@@ -15,12 +17,16 @@ class HASSStatus:
     configHASS = None
     debugLevel = 0
     master = None
-    msgRate = {}
-    msgRatePerSensor = 60
+    msgRateSeconds = 15
+    resendRateInSeconds = 3600 
+    retryRateInSeconds = 60
+    msgQueue = {}    
     status = False
     serverIP = None
     serverPort = 8123
     timeout = 2
+    backgroundTasksLock = threading.Lock()
+    backgroundTasksThread = threading.Thread(target=background_task_thread, args=())
 
     def __init__(self, master):
         self.config = master.config
@@ -37,87 +43,111 @@ class HASSStatus:
         self.serverIP = self.configHASS.get("serverIP", None)
         self.serverPort = self.configHASS.get("serverPort", 8123)
         self.apiKey = self.configHASS.get("apiKey", None)
+        self.msgRateSeconds = self.configHASS.get("msgRateSeconds", 15)
+        self.resendRateInSeconds = self.configHASS.get("resendRateInSeconds", 3600)
+        self.retryRateInSeconds = self.configHASS.get("retryRateInSeconds", 60)
         self.debugLevel = self.configConfig.get("debugLevel", 0)
 
         # Unload if this module is disabled or misconfigured
         if ((not self.status) or (not self.serverIP)
            or (int(self.serverPort) < 1) or (not self.apiKey)):
-          self.master.releaseModule("lib.TWCManager.Status","HASSStatus");
+            self.master.releaseModule("lib.TWCManager.Status","HASSStatus")     
+        else:          
+            self.backgroundTasksThread.daemon = True
+            self.backgroundTasksThread.start()
 
+    def getTwident(self, twcid):
+        # Format TWCID nicely
+        if len(twcid) == 2:
+            return "%02X%02X" % (twcid[0], twcid[1])
+        else:
+            return str(twcid.decode("utf-8"))
+
+    def background_task_thread(self):
+        while True:
+            self.time.sleep(self.msgRateSeconds)
+            self.backgroundTasksLock.acquire()
+            for msg in self.msgQueue:
+                if msg["elapsingTime"] < self.time.time():
+                    self.sendingStatusToHASS(msg["sensor"], msg["twcid"], msg["key_underscore"], msg["key_camelcase"], msg["value"], msg["unit"])
+            self.backgroundTasksLock.release()
+
+    def getSensorName(self, twcid, key_underscore):
+        return "sensor.twcmanager_" + str(self.getTwident(twcid)) + "_" + key_underscore
 
     def setStatus(self, twcid, key_underscore, key_camelcase, value, unit):
-
-        # Format TWCID nicely
-        twident = None
-        if len(twcid) == 2:
-            twident = "%02X%02X" % (twcid[0], twcid[1])
-        else:
-            twident = str(twcid.decode("utf-8"))
-
-        sensor = "sensor.twcmanager_" + str(twident) + "_" + key_underscore
-
-        if self.status:
-
-            # Perform rate limiting first (as there are some very chatty topics).
-            # For each message that comes through, we take the sensor name and check
-            # when we last updated it. If it was less than msgRatePerSensor
-            # seconds ago, we dampen it.
-            if sensor in self.msgRate:
-                if (self.time.time() - self.msgRate[sensor]) < self.msgRatePerSensor:
-                    return True
-                else:
-                    self.msgRate[sensor] = self.time.time()
-            else:
-                self.msgRate[sensor] = self.time.time()
-
-            url = "http://" + self.serverIP + ":" + self.serverPort
-            url = url + "/api/states/" + sensor
-            headers = {
-                "Authorization": "Bearer " + self.apiKey,
-                "content-type": "application/json",
+        self.backgroundTasksLock.acquire()
+        sensor = self.getSensorName(twcid, key_underscore)  
+        self.msgQueue[sensor] = {
+                "elapsingTime": self.time.time(), 
+                "sensor": sensor, 
+                "twcid": twcid, 
+                "key_underscore": key_underscore, 
+                "key_camelcase": key_camelcase, 
+                "value": value, 
+                "unit": unit 
             }
+        self.backgroundTasksLock.release()
 
-            try:
-                self.master.debugLog(
-                    8,
-                    "HASSStatus",
-                    f(
-                        "Sending POST request to HomeAssistant for sensor {sensor} (value {value})."
-                    ),
-                )
-                devclass = ""
-                if  str.upper(unit) in ["W","A","V","KWH"]:
-                    devclass="power"
+    def sendingStatusToHASS(self, sensor, twcid, key_underscore, key_camelcase, value, unit):
 
-                if len(unit)>0:
-                    self.requests.post(
-                        url, json={"state": value, "attributes": { "unit_of_measurement": unit, "device_class": devclass, "friendly_name": "TWC " + str(twident) + " " + key_camelcase } }, timeout=self.timeout, headers=headers
-                    )
-                else:
-                    self.requests.post(
-                        url, json={"state": value, "attributes": { "friendly_name": "TWC " + str(twident) + " " + key_camelcase } }, timeout=self.timeout, headers=headers
-                    )
-            except self.requests.exceptions.ConnectionError as e:
-                self.master.debugLog(
-                    4,
-                    "HASSStatus",
-                    "Error connecting to HomeAssistant to publish sensor values",
+        url = "http://" + self.serverIP + ":" + self.serverPort
+        url = url + "/api/states/" + sensor
+        headers = {
+            "Authorization": "Bearer " + self.apiKey,
+            "content-type": "application/json",
+        }
+        try:
+            self.master.debugLog(
+                8,
+                "HASSStatus",
+                f(
+                    "Sending POST request to HomeAssistant for sensor {sensor} (value {value})."
+                ),
+            )
+
+            devclass = ""
+            if  str.upper(unit) in ["W","A","V","KWH"]:
+                devclass="power"
+
+            if len(unit)>0:
+                self.requests.post(
+                    url, json={"state": value, "attributes": { "unit_of_measurement": unit, "device_class": devclass, "friendly_name": "TWC " + str(self.getTwident(twcid)) + " " + key_camelcase } }, timeout=self.timeout, headers=headers
                 )
-                self.master.debugLog(10, "HASSStatus", str(e))
-                return False
-            except self.requests.exceptions.ReadTimeout as e:
-                self.master.debugLog(
-                    4,
-                    "HASSStatus",
-                    "Error connecting to HomeAssistant to publish sensor values",
+            else:
+                self.requests.post(
+                    url, json={"state": value, "attributes": { "friendly_name": "TWC " + str(self.getTwident(twcid)) + " " + key_camelcase } }, timeout=self.timeout, headers=headers
                 )
-                self.master.debugLog(10, "HASSStatus", str(e))
-                return False
-            except Exception as e:
-                self.master.debugLog(
-                    4,
-                    "HASSStatus",
-                    "Error during publishing HomeAssistant sensor values",
-                )
-                self.master.debugLog(10, "HASSStatus", str(e))
-                return False
+            # Setting elapsing time to now + resendRateInSeconds
+            self.msgQueue[sensor]["elapsingTime"] = self.time.time() + self.resendRateInSeconds               
+        except self.requests.exceptions.ConnectionError as e:
+            self.master.debugLog(
+                4,
+                "HASSStatus",
+                "Error connecting to HomeAssistant to publish sensor values",
+            )
+            self.master.debugLog(10, "HASSStatus", str(e))
+            self.settingRetryRate(sensor)
+            return False
+        except self.requests.exceptions.ReadTimeout as e:
+            self.master.debugLog(
+                4,
+                "HASSStatus",
+                "Error connecting to HomeAssistant to publish sensor values",
+            )
+            self.master.debugLog(10, "HASSStatus", str(e))
+            self.settingRetryRate(sensor)
+            return False
+        except Exception as e:
+            self.master.debugLog(
+                4,
+                "HASSStatus",
+                "Error during publishing HomeAssistant sensor values",
+            )
+            self.master.debugLog(10, "HASSStatus", str(e))
+            self.settingRetryRate(sensor)
+            return False
+
+    def settingRetryRate(self, sensor):
+        # Setting elapsing time to now + retryRateInSeconds
+        self.msgQueue[sensor]["elapsingTime"] = self.time.time() + self.retryRateInSeconds     
