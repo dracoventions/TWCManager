96980867a52e651391ad4770081d830b00cddc0b
hopfi2k@me.com <+Nick4nack.08>
2020-11-02 11:30:31 +0100

    include TWCManager version to use most up to date move favicon

--- lib/TWCManager/Control/HTTPControl.py
+++ lib/TWCManager/Control/HTTPControl.py
@@ -66,7 +66,8 @@ class HTTPControlHandler(BaseHTTPRequestHandler):
         <link rel='stylesheet' href='https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css' integrity='sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T' crossorigin='anonymous'>
         """
         page += (
-                "<link rel='icon' type='image/png' href='https://raw.githubusercontent.com/ngardiner/TWCManager/master/tree/%s/html/favicon.png'>"
+                "<link rel='icon' type='image/png' href='https://raw.githubusercontent.com/ngardiner/TWCManager"
+                "/master/tree/%s/html/favicon.png'> "
                 % self.version
         )
         return page
