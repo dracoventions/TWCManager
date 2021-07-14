86a1e5c29d13f1817a6d670c66cddc62694fb58c
Mark van Eijken <markvaneijken@gmail.com>
2021-07-14 20:47:10 +0200

    Cleanup P1Monitor

--- lib/TWCManager/EMS/P1Monitor.py
+++ lib/TWCManager/EMS/P1Monitor.py
@@ -8,59 +8,23 @@ class P1Monitor:
     import json
     import requests
 
-    config = None
-    configConfig = None
     consumedW = 0
-    fetchFailed = False
     generatedW = 0
-    master = None
     timeout = 10
 
     def __init__(self, master):
-        self.master = master
-        self.config = master.config
-        try:
-            self.configConfig = master.config["config"]
-        except KeyError:
-            self.configConfig = {}
+
         try:
             self.configP1Mon = master.config["sources"]["P1Monitor"]
             self.serverIP = self.configP1Mon.get("serverIP", None)
-        except KeyError:
-            logger.error("Cannot find serverIP for P1Monitor in config.json")
-        try:
-            self.configP1Mon = master.config["sources"]["P1Monitor"]
-            self.mode = self.configP1Mon.get("mode", "TrackGreenEnergy")
-        except KeyError:
-            logger.error("Cannot find mode (LoadBalancing/TrackGreenEnergy) for P1Monitor in config.json")
-
-        try:
-            self.configP1Mon = master.config["sources"]["P1Monitor"]
-            self.maxAmpsPerPhase = self.configP1Mon.get("maxAmpsPerPhase", 10)
-        except KeyError:
-            logger.error("Cannot find maxAmpsPerPhase value for P1Monitor in config.json")
+        except (KeyError) as e:
+            logger.error("Cannot get configuration for P1Monitor in config.json", e)
 
         # Unload if this module is disabled or misconfigured
         if not self.serverIP:
-            logger.warning("Cannot use P1Monitor module bacause it has no server ip configured!")
+            logger.error("Cannot use P1Monitor module bacause it has no server ip configured!")
             self.master.releaseModule("lib.TWCManager.EMS", "P1Monitor")
             return None
-        if self.mode.tolower() not in ["loadbalancing","trackgreenenergy"] :
-            logger.warning("Mode must be set to LoadBalancing or TrackGreenEnergy")
-            self.master.releaseModule("lib.TWCManager.EMS", "P1Monitor")
-            return None
-
-    def maxWatt(self):
-        p1monData = self.getP1MonAPIData()
-        L1_V = int(float(p1monData[0]['L1_V']))
-        L2_V = int(float(p1monData[0]['L2_V']))
-        L3_V = int(float(p1monData[0]['L3_V']))
-
-        return max([
-            L1_V * int(self.maxAmpsPerPhase),
-            L2_V * int(self.maxAmpsPerPhase),
-            L3_V * int(self.maxAmpsPerPhase)
-            ])
 
     def getConsumption(self):
 
@@ -88,76 +52,21 @@ class P1Monitor:
         try:
             r = self.requests.get(url, timeout=self.timeout)
         except self.requests.exceptions.ConnectionError as e:
-            logger.error(
-                4,
-                "P1Monitor",
-                "Error connecting to P1Monitor API to fetch sensor value",
-            )
-            self.fetchFailed = True
+            logger.error("P1Monitor: Error connecting to P1Monitor API to fetch sensor value")
             return False
 
         try:
             r.raise_for_status()
         except self.requests.exceptions.HTTPError as e:
-            logger.error(
-                4,
-                "P1Monitor",
-                "HTTP status "
-                + str(e.response.status_code)
-                + " connecting to P1Monitor API to fetch sensor value",
-            )
+            logger.error("P1Monitor: HTTP status " + str(e.response.status_code) + " connecting to P1Monitor API to fetch sensor value")
 
         return r.json()
 
     def update(self):
-        # This module support loadbalancing. So when mode is set to LoadBalancing it will manipulate the generatedW to
-        # force TWCManager pumping up the amps to achive the maximum amount of Amps available on the net.
-        # By default the maximum amount of Amps is set to 10A.
 
         p1monData = self.getP1MonAPIData()
         if p1monData:
             try:
-                if self.mode.lower() == "loadbalancing":
-
-                    L1_V = int(float(p1monData[0]['L1_V']))
-                    L2_V = int(float(p1monData[0]['L2_V']))
-                    L3_V = int(float(p1monData[0]['L3_V']))
-
-                    maxWattAvailable = int(
-                        max([
-                            L1_V * int(self.maxAmpsPerPhase),
-                            L2_V * int(self.maxAmpsPerPhase),
-                            L3_V * int(self.maxAmpsPerPhase)
-                            ])
-                    )
-
-                    consumedW = int(
-                        max([
-                            int(float(p1monData[0]['CONSUMPTION_L1_W'])),
-                            int(float(p1monData[0]['CONSUMPTION_L2_W'])),
-                            int(float(p1monData[0]['CONSUMPTION_L3_W']))
-                            ])
-                    )
-                    generatedW = int(
-                        max([
-                            int(float(p1monData[0]['PRODUCTION_L1_W'])),
-                            int(float(p1monData[0]['PRODUCTION_L2_W'])),
-                            int(float(p1monData[0]['PRODUCTION_L3_W']))
-                            ])
-                    )
-
-                    if consumedW >= maxWattAvailable:
-                        availableW = 0
-                    else:
-                        availableW = maxWattAvailable - (consumedW + generatedW)
-
-                    self.consumedW = sum(consumedW )
-                    self.generatedW = sum(consumedW )
-
-                else:
-                    #TrackGreenEnergy
-                    # We don't want to overload each phase. Therefor we'll use the maxAmpsPerPhase config.json attribute to check against.
-
                     self.consumedW = int(
                         max([
                             int(float(p1monData[0]['CONSUMPTION_L1_W'])),
@@ -166,27 +75,16 @@ class P1Monitor:
                             ])
                     )
                     self.generatedW = int(
-                        #Sum all values because we want make use of all power generated
-                        sum([
+                        max([
                             int(float(p1monData[0]['PRODUCTION_L1_W'])),
                             int(float(p1monData[0]['PRODUCTION_L2_W'])),
                             int(float(p1monData[0]['PRODUCTION_L3_W']))
                             ])
                     )
             except (KeyError, TypeError) as e:
-                logger.debug(
-                    4,
-                    "P1Monitor",
-                    "Exception during parsing P1Monitor data (consumedW)",
-                )
-                logger.debug(10, "P1Monitor", e)
+                logger.error("P1Monitor: Exception during parsing P1Monitor data", e)
         else:
-            logger.debug(
-                4,
-                "P1Monitor",
-                "P1Monitor API result does not contain json content.",
-            )
-            self.fetchFailed = True
+            logger.error("P1Monitor: P1Monitor API result does not contain json content")
 
         return True
 
