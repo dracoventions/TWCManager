86de3daf1c4ff1e69051a4494ff0f26db7426b56
Mark van Eijken <markvaneijken@gmail.com>
2021-07-15 13:54:04 +0200

    Update docs, implement trimmed average to avoid reporting spikes.

--- lib/TWCManager/EMS/P1Monitor.py
+++ lib/TWCManager/EMS/P1Monitor.py
@@ -1,6 +1,11 @@
 ﻿# P1 Monitor API integration  (https://www.ztatz.nl/)
 
+# This module takes an average of the last X (min 1, max 10) values deliverd by the P1 Monitor API.
+# This makes that any spikes are not directly influence the behavior of TWCManager.
+
 import logging
+import array
+import scipy.stats
 logger = logging.getLogger(__name__.rsplit(".")[-1])
 
 class P1Monitor:
@@ -17,13 +22,20 @@ class P1Monitor:
         try:
             self.configP1Mon = master.config["sources"]["P1Monitor"]
             self.serverIP = self.configP1Mon.get("serverIP", None)
+            self.samples = self.configP1Mon.get("samples", 1)
         except (KeyError) as e:
             logger.error("Cannot get configuration for P1Monitor in config.json", e)
 
         # Unload if this module is disabled or misconfigured
         if not self.serverIP:
             logger.error("Cannot use P1Monitor module bacause it has no server ip configured!")
-            self.master.releaseModule("lib.TWCManager.EMS", "P1Monitor")
+            master.releaseModule("lib.TWCManager.EMS", "P1Monitor")
+            return None
+
+        # Unload if this module is disabled or misconfigured
+        if self.samples < 1 or self.samples > 10 :
+            logger.error("Cannot use P1Monitor module bacause the samples configured in config.json is not a value from 1 to 10!")
+            master.releaseModule("lib.TWCManager.EMS", "P1Monitor")
             return None
 
     def getConsumption(self):
@@ -32,7 +44,10 @@ class P1Monitor:
         self.update()
 
         # Return current consumed value
-        return float(self.consumedW)
+        if self.consumedW > 0:
+            return float(self.consumedW)
+        else:
+            return float(0)
 
     def getGeneration(self):
 
@@ -40,14 +55,18 @@ class P1Monitor:
         self.update()
 
         # Return generation value
-        return float(self.generatedW)
+        if self.generatedW > 0:
+            return float(self.generatedW)
+        else:
+            return float(0)
+
 
     def getP1MonAPIData(self):
 
         # Fetch the specified data from the P1Monitor API and return the data
         self.fetchFailed = False
 
-        url = "http://" + self.serverIP + "/api/v1/phase?limit=1&json=object&round=on"
+        url = "http://" + self.serverIP + "/api/v1/phase?limit=" + str(self.samples) + "&json=object&round=on"
 
         try:
             r = self.requests.get(url, timeout=self.timeout)
@@ -67,18 +86,30 @@ class P1Monitor:
         p1monData = self.getP1MonAPIData()
         if p1monData:
             try:
+
+                    # Calculate the avarage trimming 10% of the highest and lowest values https://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.trim_mean.html
+                    CONSUMPTION_L1_W_Avg = scipy.stats.trim_mean(array.array('i',(int(float(p1monData[i]['CONSUMPTION_L1_W'])) for i in range(0,self.samples))),0.1)
+                    CONSUMPTION_L2_W_Avg = scipy.stats.trim_mean(array.array('i',(int(float(p1monData[i]['CONSUMPTION_L2_W'])) for i in range(0,self.samples))),0.1)
+                    CONSUMPTION_L3_W_Avg = scipy.stats.trim_mean(array.array('i',(int(float(p1monData[i]['CONSUMPTION_L3_W'])) for i in range(0,self.samples))),0.1)
+                    PRODUCTION_L1_W_Avg = scipy.stats.trim_mean(array.array('i',(int(float(p1monData[i]['PRODUCTION_L1_W'])) for i in range(0,self.samples))),0.1)
+                    PRODUCTION_L2_W_Avg = scipy.stats.trim_mean(array.array('i',(int(float(p1monData[i]['PRODUCTION_L2_W'])) for i in range(0,self.samples))),0.1)
+                    PRODUCTION_L3_W_Avg = scipy.stats.trim_mean(array.array('i',(int(float(p1monData[i]['PRODUCTION_L3_W'])) for i in range(0,self.samples))),0.1)
+
+                    #Get the max value of consumption, because we don't want to overload the fuse.
                     self.consumedW = int(
                         max([
-                            int(float(p1monData[0]['CONSUMPTION_L1_W'])),
-                            int(float(p1monData[0]['CONSUMPTION_L2_W'])),
-                            int(float(p1monData[0]['CONSUMPTION_L3_W']))
+                            CONSUMPTION_L1_W_Avg,
+                            CONSUMPTION_L2_W_Avg,
+                            CONSUMPTION_L3_W_Avg
                             ])
                     )
+                    # Get the sum value of production, because it should be installed within the fuse configuration
+                    # and we want to make use of all the power we produce.
                     self.generatedW = int(
-                        max([
-                            int(float(p1monData[0]['PRODUCTION_L1_W'])),
-                            int(float(p1monData[0]['PRODUCTION_L2_W'])),
-                            int(float(p1monData[0]['PRODUCTION_L3_W']))
+                        sum([
+                            PRODUCTION_L1_W_Avg,
+                            PRODUCTION_L2_W_Avg,
+                            PRODUCTION_L3_W_Avg
                             ])
                     )
             except (KeyError, TypeError) as e:
