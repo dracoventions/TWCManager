8471730410fdf3692e21468572554522b2765cc7
Nathan Gardiner <ngardiner@gmail.com>
2021-07-17 18:40:31 +1000

    Fix - don't fail if pwd is not part of Python path

--- TWCManager.py
+++ TWCManager.py
@@ -4,6 +4,10 @@ import os
 import sys
 
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + "/lib")
-sys.path.remove(os.path.dirname(os.path.realpath(__file__)))
+try:
+   sys.path.remove(os.path.dirname(os.path.realpath(__file__)))
+except ValueError:
+    # Current directory is not in path - ignore
+    continue
 
 import TWCManager.TWCManager
