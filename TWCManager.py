39120d67c36745710eba986715c224a8cbaf0429
Nathan Gardiner <ngardiner@gmail.com>
2021-07-17 19:28:00 +1000

    Fixes for CI failures

--- TWCManager.py
+++ TWCManager.py
@@ -7,7 +7,10 @@ sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + "/lib")
 try:
    sys.path.remove(os.path.dirname(os.path.realpath(__file__)))
 except ValueError:
-    # Current directory is not in path - ignore
-    pass
+    # Current directory is not in path. Show current path
+    print("It appears you may have a different setup - if you get an error that TWCManager is not a package, please paste this output into a GitHub issue:")
+    print("Current Path:" + os.path.realpath(__file__))
+    print("Python Path:" + sys.path)
+    #pass
 
 import TWCManager.TWCManager
