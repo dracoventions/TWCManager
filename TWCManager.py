84927a68263460501675392388dbad48f7d86dde
Nathan Gardiner <ngardiner@gmail.com>
2021-07-19 17:43:34 +1000

    CI improvements

--- TWCManager.py
+++ TWCManager.py
@@ -9,18 +9,17 @@ sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + "/lib")
 # see an error when we try to import TWCManager.TWCManager, as it will see
 # us (TWCManager.py) instead of the package (lib/TWCManager) and fail.
 if '' in sys.path:
-    os.path.remove('')
+    sys.path.remove('')
 
 if '.' in sys.path:
-    os.path.remove('.')
+    sys.path.remove('.')
 
-try:
-   sys.path.remove(os.path.dirname(os.path.realpath(__file__)))
-except ValueError:
-    # Current directory is not in path. Show current path
-    print("It appears you may have a different setup - if you get an error that TWCManager is not a package, please paste this output into a GitHub issue:")
+if os.path.dirname(os.path.realpath(__file__)) in sys.path:
+    print("Fully-qualified path is present")
+    sys.path.remove(os.path.dirname(os.path.realpath(__file__)))
+else:
+    print("Fully-qualified path is not present")
     print("Current Path:" + os.path.realpath(__file__))
     print("Python Path:" + str(sys.path))
-    #pass
 
 import TWCManager.TWCManager
