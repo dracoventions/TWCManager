a61bb2b8e2add84bf619c2d95ba25927c9885fcf
Nathan Gardiner <ngardiner@gmail.com>
2021-07-19 17:28:32 +1000

    CI improvements

--- TWCManager.py
+++ TWCManager.py
@@ -4,6 +4,16 @@ import os
 import sys
 
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__)) + "/lib")
+
+# Remove any local local path references in sys.path, otherwise we'll
+# see an error when we try to import TWCManager.TWCManager, as it will see
+# us (TWCManager.py) instead of the package (lib/TWCManager) and fail.
+if '' in sys.path:
+    os.path.remove('')
+
+if '.' in sys.path:
+    os.path.remove('.')
+
 try:
    sys.path.remove(os.path.dirname(os.path.realpath(__file__)))
 except ValueError:
