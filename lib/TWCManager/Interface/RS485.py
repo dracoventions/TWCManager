b598bd0ce27f34b67414508d7c96b3ff73ab9f26
Nathan Gardiner <ngardiner@gmail.com>
2022-08-26 07:16:29 +1000

    Reconnect serial interface on error

--- lib/TWCManager/Interface/RS485.py
+++ lib/TWCManager/Interface/RS485.py
@@ -48,6 +48,9 @@ class RS485:
         elif porta:
             self.port = porta
 
+        self.connect()
+
+    def connect(self):
         # Connect to serial port
         self.ser = self.serial.serial_for_url(self.port, self.baud, timeout=0)
 
@@ -62,7 +65,11 @@ class RS485:
 
     def read(self, len):
         # Read the specified amount of data from the serial interface
-        return self.ser.read(len)
+        try:
+            return self.ser.read(len)
+        except serial.serialutil.SerialException as e:
+            logger.log(logging.ERROR, "Error reading from serial interface: {}. Will attempt re-connect.".format(e))
+            self.connect()
 
     def send(self, msg):
         # Send msg on the RS485 network. We'll escape bytes with a special meaning,
